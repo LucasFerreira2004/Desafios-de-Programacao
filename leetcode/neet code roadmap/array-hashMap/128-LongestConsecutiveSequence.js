b782b91df7d18d092670088b7a9ff7b6a182a4ad
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-04-07 23:30:15 -0300

    leetcode-128-longestConsecutiveSequence

---
+++ leetcode/neet code roadmap/array-hashMap/128-LongestConsecutiveSequence.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const nums = new Array(100, 4, 200, 1,3,2);
+
+var longestConsecutive = function(nums) {
+    let set = new Set(nums);
+    let maxSequence = 0;
+
+    for(let num of set){
+        if(set.has(num-1)) continue;
+        let sequence = 1;
+        while(set.has(num + sequence)){
+            sequence++;
+        }
+        maxSequence = Math.max(maxSequence, sequence);
+    }
+
+    return maxSequence;
+};
+
+console.log(longestConsecutive(nums));
